7c33ce0ea873e949960f23ea2c6160c952b85fed
Thibeau Duerinck <thibeau.duerinck@icloud.com>
2024-11-19 15:21:01 +0100

    Update styling

--- src/app/reversestring/reversestring.component.ts
+++ src/app/reversestring/reversestring.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 
+interface AnimatedChar {
+  value: string;
+  final: string;
+  reversed: boolean;
+}
+
 @Component({
   selector: "app-reversestring",
   templateUrl: "./reversestring.component.html",
@@ -8,12 +14,36 @@ import { Component, OnInit } from "@angular/core";
 export class ReversestringComponent implements OnInit {
   inputString: string = "";
   reversedString: string = "";
+  animatedChars: AnimatedChar[] = [];
+  readonly CHAR_DELAY = 100;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   reverseString() {
+    if (!this.inputString) return;
+
     this.reversedString = this.inputString.split("").reverse().join("");
+    this.animateReverse();
+  }
+
+  animateReverse() {
+    // Initialize animation array with original characters
+    this.animatedChars = this.inputString.split("").map((char) => ({
+      value: char,
+      final: char,
+      reversed: false,
+    }));
+
+    const finalPositions = this.reversedString.split("");
+
+    // Animate each character
+    this.animatedChars.forEach((char, index) => {
+      setTimeout(() => {
+        char.value = finalPositions[index];
+        char.reversed = true;
+      }, index * this.CHAR_DELAY);
+    });
   }
 }
